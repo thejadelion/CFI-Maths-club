76df939d6243ee4c2c468b2995f2448197cf56f4
Kailash Gopal D <121489805+thejadelion@users.noreply.github.com>
2023-03-08 23:13:35 +0530

    Add files via upload
    with gaussian

---
+++ try2.py
@@ -0,0 +1,86 @@
+import random
+import math
+
+
+
+def iterate(csys, velocities, dt):
+    # This function is to update the position of the particle in each iteration, taking into account boundaries
+    for i in range(n_particles):
+        csys[i][0] = velocities[i][0] * dt + csys[i][0]
+        csys[i][1] = velocities[i][1] * dt + csys[i][1]
+        if csys[i][0] > 1:
+            csys[i][0] = 2 - csys[i][0]
+            velocities[i][0] =- velocities[i][0]
+        if csys[i][1] > 1:
+            csys[i][1] = 2 - csys[i][1]
+            velocities[i][1] = -velocities[i][1]
+        if csys[i][0] < 0:
+            csys[i][0] = - csys[i][0]
+            velocities[i][0] = -velocities[i][0]
+        if csys[i][1] < 0:
+            csys[i][1] = - csys[i][1]
+            velocities[i][1] = -velocities[i][1]
+    return csys
+
+
+
+def collide(csys1, csys2):
+    # This function is to update the number of collisions in each iteration.
+    # Removing the particles which have already collided (maybe with probability)
+    count = 0
+    clist = []
+    global n_particles
+    for i in range(n_particles):
+        for j in range(i):
+            if math.dist(csys1[i], csys1[j]) < 2*r or math.dist(csys2[i], csys2[j]) < 2*r:
+                count += 1
+                d = random.random()
+                if d < 0.3:
+                    if i not in clist:
+                        clist.append(i)
+                    if j not in clist:
+                        clist.append(j)
+    ctr = 0
+    n_particles -= len(clist)
+    for i in clist:
+        csys2.pop(i-ctr)
+        ctr += 1
+    return count
+
+def controller(t):
+    tnow = 0
+    dt = 0.0001
+    collisions = 0
+
+    while tnow < t:
+        csys1 = list(csys)
+        iterate(csys, velocities, dt)
+        csys2 = csys
+        cin = collisions
+        collisions += collide(csys1, csys2)
+        tnow += dt
+    return collisions/tnow
+
+
+for i1 in range(1, 100):
+    t = 10*i1
+    r = 0.002
+    n_particles = 500
+    csys = []
+    for i in range(n_particles):
+        csys.append([random.random(), random.random()])
+
+    velocities = []
+    for i in range(n_particles):
+        vx = random.gauss(math.sqrt(t), 0.47*math.sqrt(t))
+        if vx > 100:
+            vx = 100
+        vy = random.gauss(math.sqrt(t), 0.47*math.sqrt(t))
+        if vy > 100:
+            vy = 100
+        v1 = math.sqrt(vx ** 2 + vy ** 2)
+        vx, vy = math.sqrt(t) * vx / v1, math.sqrt(t) * vy / v1
+        velocities.append([vx, vy])
+
+    print(t, controller(0.01))
+
